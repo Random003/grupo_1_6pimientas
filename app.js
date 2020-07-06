a7a05e508a737b5c3b047949acba12acad775b77
Random003 <mealla.jp@gmail.com>
2020-07-06 20:44:39 -0300

    installado express y agregado a app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Asi aparece en GitHub la carpeta");
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.listen(4005, () => { console.log("Funciona, puerto 4005")})
