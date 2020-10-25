117bd166c3233e0498a6960fd839dc302d8a1e5d
LuisRufino <luismrufino@gmail.com>
2020-10-25 13:46:49 -0300

    Generación de Api total de ventas e integración con  Dashborad

--- dashboard/src/components/mainContainer/AmountSales.js
+++ dashboard/src/components/mainContainer/AmountSales.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function AmountSales() {
-    return (
-        <React.Fragment>
-            <div className="col-md-4 mb-4">
-                <div className="card border-left-success shadow h-100 py-2">
-                    <div className="card-body">
-                        <div className="row no-gutters align-items-center">
-                            <div className="col mr-2">
-                                <div className="text-xs font-weight-bold text-success text-uppercase mb-1">Total Ventas</div>
-                                <div className="h5 mb-0 font-weight-bold text-gray-800">$546.456</div>
-                            </div>
-                            <div className="col-auto">
-                                <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+class AmountSales extends Component {
+    constructor() {
+        super();
+        this.state = {
+            amountSales: 0
+        };
+    }
+
+
+    componentDidMount() {
+
+        fetch("http://localhost:5001/api/products/amountSales", { method: "POST" })
+            .then(response => response.json())
+            .then(data => {
+
+                this.setState(
+                    {
+                        amountSales: data.data.amountSales,
+
+                    }
+                )
+            })
+
+            .catch(error => console.log(error));
+
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="col-md-4 mb-4">
+                    <div className="card border-left-success shadow h-100 py-2">
+                        <div className="card-body">
+                            <div className="row no-gutters align-items-center">
+                                <div className="col mr-2">
+                                    <div className="text-xs font-weight-bold text-success text-uppercase mb-1">Total Ventas</div>
+                                    <div className="h5 mb-0 font-weight-bold text-gray-800">${this.state.amountSales}</div>
+                                </div>
+                                <div className="col-auto">
+                                    <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </React.Fragment>
-    );
+            </React.Fragment>
+        );
+
+    }
 }
 
 export default AmountSales;
